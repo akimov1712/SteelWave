c8cff10fe953426a698e0cfc9ba8140549cf87f3
akimov1721 <akimov1712@mail.ru>
2023-09-02 14:22:28 +0300

    Add implementation repositories in data layer

--- app/src/main/java/ru/steelwave/steelwave/domain/useCase/user/GetAllUserUseCase.kt
+++ app/src/main/java/ru/steelwave/steelwave/domain/useCase/user/GetAllUserUseCase.kt
@@ -1,5 +1,6 @@
 package ru.steelwave.steelwave.domain.useCase.user
 
+import ru.steelwave.steelwave.domain.repository.user.UserRepository
 import javax.inject.Inject
 
 class GetAllUserUseCase @Inject constructor(private val repository: UserRepository) {
--- app/src/main/java/ru/steelwave/steelwave/domain/useCase/user/GetUserUseCase.kt
+++ app/src/main/java/ru/steelwave/steelwave/domain/useCase/user/GetUserUseCase.kt
@@ -1,5 +1,6 @@
 package ru.steelwave.steelwave.domain.useCase.user
 
+import ru.steelwave.steelwave.domain.repository.user.UserRepository
 import javax.inject.Inject
 
 class GetUserUseCase @Inject constructor(private val repository: UserRepository) {
