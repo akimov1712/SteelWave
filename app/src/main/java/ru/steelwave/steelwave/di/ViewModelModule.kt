42c1a8fa0f508901b89b871ea9656653241f121e
akimov1721 <akimov1712@mail.ru>
2023-09-02 14:39:55 +0300

    add Binds in ViewModelModule

--- app/src/main/java/ru/steelwave/steelwave/di/ViewModelModule.kt
+++ app/src/main/java/ru/steelwave/steelwave/di/ViewModelModule.kt
@@ -1,10 +1,47 @@
 package ru.steelwave.steelwave.di
 
+import androidx.lifecycle.ViewModel
+import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
+import ru.steelwave.steelwave.presentation.auth.AuthViewModel
+import ru.steelwave.steelwave.presentation.main.ads.AdsViewModel
+import ru.steelwave.steelwave.presentation.main.employees.EmployeesViewModel
+import ru.steelwave.steelwave.presentation.main.finance.FinanceViewModel
+import ru.steelwave.steelwave.presentation.main.project.ProjectViewModel
+import ru.steelwave.steelwave.presentation.main.trafic.TraficViewModel
 
 @Module
 interface ViewModelModule {
 
+    @IntoMap
+    @ViewModelKey(AdsViewModel::class)
+    @Binds
+    fun bindAdsViewModel(viewModel: AdsViewModel): ViewModel
 
+    @IntoMap
+    @ViewModelKey(EmployeesViewModel::class)
+    @Binds
+    fun bindEmployeesViewModel(viewModel: EmployeesViewModel): ViewModel
+
+    @IntoMap
+    @ViewModelKey(FinanceViewModel::class)
+    @Binds
+    fun bindFinanceViewModel(viewModel: FinanceViewModel): ViewModel
+
+    @IntoMap
+    @ViewModelKey(ProjectViewModel::class)
+    @Binds
+    fun bindProjectViewModel(viewModel: ProjectViewModel): ViewModel
+
+    @IntoMap
+    @ViewModelKey(TraficViewModel::class)
+    @Binds
+    fun bindTraficViewModel(viewModel: TraficViewModel): ViewModel
+
+    @IntoMap
+    @ViewModelKey(AuthViewModel::class)
+    @Binds
+    fun bindAuthViewModel(viewModel: AuthViewModel): ViewModel
 
 }
---
+++ app/src/main/java/ru/steelwave/steelwave/presentation/auth/AuthViewModel.kt
@@ -0,0 +1,7 @@
+package ru.steelwave.steelwave.presentation.auth
+
+import androidx.lifecycle.ViewModel
+
+class AuthViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
