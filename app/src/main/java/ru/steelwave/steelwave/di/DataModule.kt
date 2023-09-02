f7628e654caaa2d819711a47bbc9e97a64fa8a58
akimov1721 <akimov1712@mail.ru>
2023-09-02 14:31:20 +0300

    Add provides in DataModule

--- app/src/main/java/ru/steelwave/steelwave/di/DataModule.kt
+++ app/src/main/java/ru/steelwave/steelwave/di/DataModule.kt
@@ -1,10 +1,21 @@
 package ru.steelwave.steelwave.di
 
+import android.app.Application
 import dagger.Module
+import dagger.Provides
+import ru.steelwave.steelwave.data.database.AppDatabase
+import ru.steelwave.steelwave.data.database.dao.ProjectDao
 
 @Module
 interface DataModule {
 
+    companion object{
 
+        @Provides
+        fun provideProjectDao(application: Application): ProjectDao {
+            return AppDatabase.getInstance(application).projectDao()
+        }
+
+    }
 
 }
